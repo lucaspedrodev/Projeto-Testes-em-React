a47e06d4d52220b54eabfb92c6126c3606086cf0
Lucas Pedro <rodrigerioo@gmail.com>
2022-09-19 15:57:22 -0300

    requisito 01 concluido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,57 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente App', () => {
+  test('O topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    const linkFavorite = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+  });
+
+  test('clicar no link Home redireciona para a pagina inicial;', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    userEvent.click(linkHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('clicar no link About redireciona para a pagina sobre;', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('clicar no link Favorites redireciona para a pagina Favoritos;', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(linkFavorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('entra na pagina Not Found caso encontre uma URL desconhecida;', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/aoba');
+    });
+    const NotFound = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+
+    expect(NotFound).toBeInTheDocument();
+  });
+});
